bbea1517355ebb66b6ff0db84dbfebb88979a59f
Edgar <emarukyan@gmail.com>
2022-12-23 10:51:37 +0400

    Update make-use-of-reduce.js

--- make-use-of-reduce.js
+++ make-use-of-reduce.js
@@ -1,13 +1,14 @@
 ///////////////////////////////////////////////////////
 // Get rid of 'let's and make use of .map, .reduce ...
 
+const salesAgentRevenuePercent = 20
 
 // Example #1
 const getTheMoneyEarnedByTheAgent = (coupons) => {
   let earnedMoney = 0
   if (coupons.length !== 0) {
     return coupons.map(coupon => {
-      earnedMoney = earnedMoney + coupon.TotalInvoicePrice / 100 * config.salesAgentRevenuePercent
+      earnedMoney = earnedMoney + coupon.TotalInvoicePrice / 100 * salesAgentRevenuePercent
       return earnedMoney
     })[coupons.length - 1].toFixed(2)
   }
